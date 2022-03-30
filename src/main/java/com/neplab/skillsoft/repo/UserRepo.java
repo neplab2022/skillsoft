7bf0ffba114e00534ab3a51e16e4c84f80194363
“mandeep_dhakal” <mandip.dhakal@nad-tech.com>
2022-03-29 21:23:07 -0700

    folder structured

--- src/main/java/com/neplab/skillsoft/repo/UserRepo.java
+++ src/main/java/com/neplab/skillsoft/repo/UserRepo.java
@@ -1,8 +1,9 @@
 package com.neplab.skillsoft.repo;
 
 import com.neplab.skillsoft.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface UserRepo extends JpaRepository<User, UUID>{
+public interface UserRepo extends JpaRepository<User, UUID> {
 }
